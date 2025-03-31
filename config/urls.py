d8153da3488f37820f48fa48f03a340d505871c1
Evgeny Klyukin <key.klyukin@gmail.com>
2025-03-31 06:30:57 +0500

    feat: add the events/ path to config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
 from django.conf import settings
+from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('events/', include('apps.events.urls')),
 ]
 
 if settings.DEBUG:
